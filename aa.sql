d4d1bb67574af392e6bb16dda198fdbfac1dba16
Adwaith.H <adwaithsre@gmail.com>
2025-09-19 12:51:28 +0530

    initial commit

---
+++ aa.sql
@@ -0,0 +1,9 @@
+INSERT INTO customers (CustomerName, City, PostalCode, Country)
+VALUES ('Alfred','Berlin','12209','Germany'), 
+       ('Kiran','Delhi','05021','India'),
+       ('chong','Wuhan','S-958 22','China'),
+        ('Suresh','Mumbai','05023','India'),
+         ('Anna','London','WA1 1DP','UK'),
+          ('Ching','Beijing','S-958','China'),
+           ('Aneesh','Delhi','05021','India'),
+            ('William','Tokyo','4006','Japan');
